0fbe5f21f6c2c723edc0dcae516f44e9b5f507fa
ailinelim <85934419+ailinelim@users.noreply.github.com>
2021-09-06 17:25:34 +0800

    Update producer.py

--- lesson-3-implementing-message-passing/kafka-python-demo/producer.py
+++ lesson-3-implementing-message-passing/kafka-python-demo/producer.py
@@ -4,7 +4,7 @@ from kafka import KafkaProducer
 TOPIC_NAME = 'items'
 KAFKA_SERVER = 'my-release-kafka.default.svc.cluster.local:9092'
 
-producer = KafkaProducer(bootstrap_servers=[localhost:9092], api_version=(2, 7, 0))
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
 producer.send(TOPIC_NAME, b'Test Message!!!')
 producer.flush()
